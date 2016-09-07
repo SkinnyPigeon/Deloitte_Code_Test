343c9d6d3b26a7378690c8c27c6f70a2f30b5eee
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-09-07 13:27:13 +0100

    added the stock items

---
+++ client/src/models/stock.js
@@ -0,0 +1,97 @@
+
+
+itemOne = {
+  name: "Almond Toe Court Shoes, Patent Black",
+  category: "Women’s Footwear",
+  price: 99,
+  stock: 5
+}
+
+itemTwo = {
+  name: "Suede Shoes, Blue",
+  category: "Women’s Footwear",
+  price: 42,
+  stock: 4
+}
+
+itemThree = {
+  name: "Leather Driver Saddle Loafers, Tan",
+  category: "Men’s Footwear",
+  price: 34.00,
+  stock: 12
+}
+
+itemFour = {
+  name: "Flip Flops, Red",
+  category: "Men’s Footwear",
+  price: 19,
+  stock: 6
+}
+
+itemFive = {
+  name: "Flip Flops, Blue",
+  category: "Men’s Footwear",
+  price: 19,
+  stock: 0
+}
+
+itemSix = {
+  name: "Gold Button Cardigan, Black",
+  category: "Women’s Casualwear",
+  price: 167,
+  stock: 6
+}
+
+itemSeven = {
+  name: "Cotton Shorts, Medium Red",
+  category: "Women’s Casualwear",
+  price: 30,
+  stock: 5
+}
+
+itemEight = {
+  name: "Fine Stripe Short Sleeve Shirt, Grey",
+  category: "Men’s Casualwear",
+  price: 49.99,
+  stock: 9
+}
+
+itemNine = {
+  name: "Fine Stripe Short Sleeve Shirt, Green",
+  category: "Men’s Casualwear",
+  price: 39.99,
+  stock: 3
+}
+
+itemTen = {
+  name: "Sharkskin Waistcoat, Charcoal",
+  category: "Men’s Formalwear",
+  price: 75,
+  stock: 2
+}
+
+itemEleven = {
+  name: "Lightweight Patch Pocket Blazer, Deer",
+  category: "Men’s Formalwear ",
+  price: 175.5,
+  stock: 1
+}
+
+itemTwelve = {
+  name: "Bird Print Dress, Black",
+  category: "Women’s Formalwear",
+  price: 270,
+  stock: 10
+}
+
+itemThirteen = {
+  name: "Mid Twist Cut-Out Dress, Pink",
+  category: "Women’s Formalwear",
+  price: 540,
+  stock: 5
+}
+
+var stock = { itemOne, itemTwo, itemThree, itemFour, itemFive, itemSix, itemSeven, itemEight, itemNine, itemTen, itemEleven, itemTwelve, itemThirteen }
+
+module.exports = stock;
+
