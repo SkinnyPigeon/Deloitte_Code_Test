2d21895973c779ef6c8cd719b2f753bcfcdcfee7
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-09-07 15:10:10 +0100

    test can see the basket and is passing first test

--- client/src/specs/basket_spec.js
+++ client/src/specs/basket_spec.js
@@ -1,1 +1,11 @@
+var basket = require( '../models/basket' ).basket;
+var stock = require( '../models/stock' );
 var assert = require( 'chai' ).assert;
+
+describe( 'The Basket', function() {
+
+  it( 'Should be empty at start', function() {
+    assert.equal( 0, basket.totalItems() );
+  });
+
+});
