b8c4affff32d5dadbd4c860a9b955ddc92f279c9
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-09-07 21:07:25 +0100

    started to add the button targeting

--- client/src/app.js
+++ client/src/app.js
@@ -1,3 +1,24 @@
 window.onload = function() {
-  console.log( "app started" );
+
+  var home = document.getElementById( 'home' );
+  var women = document.getElementById( 'women' );
+  var men = document.getElementById( 'men' );
+  var basket = document.getElementById( 'basket' );
+
+  home.onclick = function(e) {
+    console.log( "clicked home" );
+  }
+
+  women.onclick = function(e) {
+    console.log( "clicked women")
+  }
+
+  men.onclick = function(e) {
+    console.log( "clicked men")
+  }
+
+  basket.onclick = function(e) {
+    console.log( "clicked basket")
+  }
 }
+
