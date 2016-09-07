02ecbaca2bdc306056f39111ab980637fe44e003
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-09-07 16:48:31 +0100

    started voucher model

---
+++ client/src/models/voucher.js
@@ -0,0 +1,27 @@
+var Voucher = function( code ) {
+  this.valid = false;
+  this.value = 0;
+  this.fiveOff = [ "AA52721", "BC67123", "GHAD782" ];
+  this.tenOff = [ "BSH7824", "BCHS927", "HAJS127" ];
+  this.fifteenOff = [ "HASK243", "ASHH326", "ADG1260" ];
+  this.code = code;
+}
+
+Voucher.prototype = {
+
+  checkIfValid: function( array, value ) {
+    for( var i = 0; i < array.length; i++ ) {
+      if( this.code === array[ i ] ) {
+        this.valid = true;
+        this.value = value;
+      }
+    }
+  },
+
+  setValidation: function() {
+    checkIfValid( this.fiveOff, 5 );
+    checkIfValid( this.tenOff, 10 );
+    checkIfValid( this.fifteenOff, 15 );
+  }
+
+}
