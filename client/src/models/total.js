cadccf23123ad20a26426fb9799ae56eca49da04
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-09-07 17:27:57 +0100

    will not apply used vouchers

--- client/src/models/total.js
+++ client/src/models/total.js
@@ -5,14 +5,23 @@ var Total = function() {
 Total.prototype = {
   setTotal: function( basket ) {
     for( var i = 0; i < basket.length; i++ ) {
-      this.total += basket[ i ].price
+      this.total += basket[ i ].price;
     }
     return this.total;
   },
 
   addVoucher: function( voucher ) {
+    if( this.checkForUsedVouchers( voucher )) {
+      return;
+    }
     if( voucher.valid && this.total >= 5 ) {
       this.total -= 5;
+    }
+  },
+
+  checkForUsedVouchers: function( voucher ) {
+    if( voucher.hasBeenUsed ) {
+      return true;
     }
   }
 
