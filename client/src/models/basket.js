2df479795b9a602f279f3a17d617e8afded26385
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-09-07 15:07:47 +0100

    added the first bit of basket functionality

---
+++ client/src/models/basket.js
@@ -0,0 +1,29 @@
+var stock = require( './stock' );
+
+var basket = {
+  items: [],
+  total: 0,
+
+  totalItems: function() {
+    return this.items.length;
+  },
+
+  add: function( item ) {
+    this.items.push( item );
+    this.total += item.price;
+  },
+
+  remove: function( item ) {
+    for( basket_item of this.items ) {
+
+      if( item.name === basket_item.name ){
+        this.total -= basket_item.price;
+      }
+      
+      if( item.name === basket_item.name ){
+        this.items.splice( basket_item.index, 1);
+      }
+    }
+  },
+}
+module.exports.basket = basket;
